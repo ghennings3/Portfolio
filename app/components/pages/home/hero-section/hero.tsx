b75f2fddd9c45cf10c4b507126f21e8eca00ebce
Gustavo Hennings <ghenn@MacBook-Air-de-Gustavo.local>
2023-06-23 21:14:13 -0300

    add cms integration

--- app/components/pages/home/hero-section/hero.tsx
+++ app/components/pages/home/hero-section/hero.tsx
@@ -5,19 +5,15 @@ import { Button } from "../../../button/button"
 import { TechBadge } from "../../../tech-badge"
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {IoLogoGithub, IoLogoLinkedin} from 'react-icons/io'
+import { HomePageInfo } from "@/app/types/page-info"
+import { RichText } from "@/app/components/rich-text/rich-text"
+import { CMSIcon } from "@/app/components/cms-icon/cms-icon"
 
-const MOCK_CONTACTS = [
-    {
-        url: 'https://github.com/ghennings3',
-        icon: <IoLogoGithub />
-    },
-    {
-        url: 'https://br.linkedin.com/in/gustavohennings',
-        icon: <IoLogoLinkedin />
-    }
-]
+type HomeSectionProps = {
+    homeInfo: HomePageInfo
+}
 
-export const HeroSection = () => {
+export const HeroSection = ({homeInfo}: HomeSectionProps) => {
     const handleContact = () => {
         const contactSection = document.querySelector('#contact')
         if(contactSection){
@@ -30,10 +26,12 @@ export const HeroSection = () => {
                 <div className="w-full lg:max-w-[530px]">
                     <p className="font-mono text-emerald-400">Olá, meu nome é</p>
                     <h2 className="text-4xl font-medium mt-2">Gustavo Hennings</h2>
-                    <p className="text-gray-400 my-6 text-sm sm:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ipsa, delectus quidem corporis rerum aliquid aspernatur est nobis vel maxime magni dolorum? Dolor, ullam?</p>
+                    <div className="text-gray-400 my-6 text-sm sm:text-base">
+                        <RichText content={homeInfo.introduction.raw} />
+                    </div>
                     <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
-                        {Array.from({length: 7}).map((_, index) => (
-                            <TechBadge name='Next.js' />
+                        {homeInfo.technologies.map((tech) => (
+                            <TechBadge name={tech.name} />
                         ))}
                     </div>
                     <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
@@ -42,9 +40,9 @@ export const HeroSection = () => {
                             <HiArrowNarrowRight size={18} />    
                         </Button>
                         <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
-                        {MOCK_CONTACTS.map((contact, index) => (
+                        {homeInfo.socials.map((contact, index) => (
                             <a href={contact.url} key={`contact ${index}`} target='_blank' className="hover:text-gray-100 transition-colors">
-                                {contact.icon}
+                                <CMSIcon icon={contact.iconSvg} />
                             </a>
                         ))}
                     </div>
@@ -52,7 +50,7 @@ export const HeroSection = () => {
                     
                 </div>
                 <div>
-                    <Image width={420} height={404} src={'/images.profile-pic.png'} alt="Profile pic" className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"/>
+                    <Image width={420} height={404} src={homeInfo.profilePicture.url} alt="Profile pic" className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"/>
                 </div>
             </div>
         </section>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
 import { HeroSection } from "./components/pages/home/hero-section/hero";
 import { HighlightedProjects } from "./components/pages/home/highlighted-projects/highlighted-projects";
 import { KnownTechs } from "./components/pages/home/known-techs/known-techs";
+import { HomePageData } from "./types/page-info";
+import { fetchHygraphQuery } from "./utils/fetch-hygraph-query";
+
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+  return fetchHygraphQuery(query, 60*60*24)
+}
 
 export default async function Home() {
+  const {page: pageData} = await getPageData()
+
+
+
   return (
     <>
-      <HeroSection />
+      <HeroSection homeInfo={pageData} />
       <KnownTechs />
       <HighlightedProjects />
     </>
